2f57298c66b99073d1da717d0356475bb2561728
arkaniio <laluahmadarkani@gmail.com>
2026-04-02 19:03:25 +0800

    add: add the new transactions repo

--- repository/transaction_repository.go
+++ repository/transaction_repository.go
@@ -1,6 +1,11 @@
 package repository
 
 import (
+	"context"
+	"errors"
+	"project-keuangan-keluarga/model"
+	"project-keuangan-keluarga/utils"
+
 	"github.com/jmoiron/sqlx"
 )
 
@@ -13,4 +18,38 @@ type repoTransaction struct {
 
 func NewTransactionRepository(db *sqlx.DB) TransactionRepository {
 	return &repoTransaction{db: db}
+}
+
+func (r *repoTransaction) CreateNewCategory(ctx context.Context, transaction *model.Transaction) error {
+
+	db_tx, err := utils.AddTransaction(r.db, ctx)
+	if err != nil {
+		return errors.New("Failed to setup the transactions settings!")
+	}
+
+	query := `
+		INSERT INTO transactions(id, user_id, type, amount, category_id, description, date, created_at, updated_at) 
+		VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);
+	`
+
+	rows, err := db_tx.ExecContext(ctx, query)
+	if err != nil {
+		return errors.New("Failed to execute the db!")
+	}
+
+	last_infected, err := rows.RowsAffected()
+	if err != nil {
+		return errors.New("Failed to get the rows affected!")
+	}
+
+	if last_infected == 0 {
+		return errors.New("Failed to insert the data!")
+	}
+
+	if err := db_tx.Commit(); err != nil {
+		return errors.New("Failed to commit the db!")
+	}
+
+	return nil
+
 }
---
+++ utils/parsing_transaction_db.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func AddTransaction(db *sqlx.DB, ctx context.Context) (*sqlx.Tx, error) {
+
+	tx_options := &sql.TxOptions{
+		ReadOnly:  false,
+		Isolation: sql.LevelSerializable,
+	}
+
+	tx, err := db.BeginTxx(ctx, tx_options)
+	if err != nil {
+		return nil, errors.New("Failed to settings the transactions")
+	}
+	defer tx.Rollback()
+
+	return tx, nil
+
+}
