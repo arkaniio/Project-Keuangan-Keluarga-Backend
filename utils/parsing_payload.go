9a82324e9c38995e20de938cd9ab83c29a73ec41
arkaniio <laluahmadarkani@gmail.com>
2026-04-02 18:12:21 +0800

    add: add the new change for a repo

--- utils/parsing_payload.go
+++ utils/parsing_payload.go
@@ -1,1 +1,43 @@
 package utils
+
+import (
+	"project-keuangan-keluarga/model"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func ParsingPayloadUser(payload model.Payload) (*model.User, error) {
+
+	hashing_password, err := HashPassword(payload.Password)
+	if err != nil {
+		return &model.User{}, err
+	}
+
+	return &model.User{
+		Id:          uuid.New(),
+		Name:        payload.Name,
+		Email:       payload.Email,
+		Password:    hashing_password,
+		Role:        payload.Role,
+		Profile_img: payload.Profile_img,
+		CreatedAt:   time.Now().UTC(),
+		UpdatedAt:   time.Now().UTC(),
+	}, nil
+}
+
+func PayloaUpdate(dest **string, val string) {
+
+	if val != "" {
+		*dest = &val
+	}
+
+}
+
+func PayloaUpdateInt64(dest **int64, val int64) {
+
+	if val != 0 {
+		*dest = &val
+	}
+
+}
