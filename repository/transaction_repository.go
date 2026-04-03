e4adab83829ac8b4c4962a691a297846b5b7fec7
arkaniio <laluahmadarkani@gmail.com>
2026-04-03 18:18:09 +0800

    add: add the repo for update transactions

--- repository/transaction_repository.go
+++ repository/transaction_repository.go
@@ -6,6 +6,7 @@ import (
 	"project-keuangan-keluarga/model"
 	"project-keuangan-keluarga/utils"
 
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -45,6 +46,31 @@ func (r *repoTransaction) CreateNewTransactions(ctx context.Context, transaction
 
 	if last_infected == 0 {
 		return errors.New("Failed to insert the data!")
+	}
+
+	if err := db_tx.Commit(); err != nil {
+		return errors.New("Failed to commit the db!")
+	}
+
+	return nil
+
+}
+
+func (r *repoTransaction) UpdateTransaction(ctx context.Context, id uuid.UUID, payload model.UpdatePayloadTransaction) error {
+
+	db_tx, err := utils.AddTransaction(r.db, ctx)
+	if err != nil {
+		return errors.New("Failed to get and settings the transactions!")
+	}
+	db_tx.Rollback()
+
+	full_query, args, err := utils.UpdateToolsTransactions(payload, id)
+	if err != nil {
+		return errors.New("Failed to get the full query for this method!")
+	}
+
+	if _, err := db_tx.ExecContext(ctx, full_query, args...); err != nil {
+		return errors.New("Failed to execute the query with context!")
 	}
 
 	if err := db_tx.Commit(); err != nil {
