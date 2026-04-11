591492b05b22afff0cd82b20e0c7ba492d7551e7
arkaniio <laluahmadarkani@gmail.com>
2026-04-11 20:07:12 +0800

    add: add new service for budget repo

--- service/budget_service.go
+++ service/budget_service.go
@@ -1,1 +1,23 @@
 package service
+
+import (
+	"context"
+	"project-keuangan-keluarga/model"
+	"project-keuangan-keluarga/repository"
+)
+
+type BudgetService interface {
+	CreateNewBudget(ctx context.Context, payload *model.Budget) error
+}
+
+type budgetService struct {
+	budgetRepo repository.BudgetRepository
+}
+
+func NewBudgetService(budgetRepo repository.BudgetRepository) BudgetService {
+	return &budgetService{budgetRepo: budgetRepo}
+}
+
+func (s *budgetService) CreateNewBudget(ctx context.Context, payload *model.Budget) error {
+	return s.budgetRepo.CreateNewBudget(ctx, payload)
+}
