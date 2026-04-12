b1838cee436bc31d63c3b34c5b19edefbc3a0dc5
arkaniio <laluahmadarkani@gmail.com>
2026-04-12 17:39:09 +0800

    add: add new services for goals repo

--- service/goals_service.go
+++ service/goals_service.go
@@ -1,1 +1,23 @@
 package service
+
+import (
+	"context"
+	"project-keuangan-keluarga/model"
+	"project-keuangan-keluarga/repository"
+)
+
+type GoalsService interface {
+	CreateNewGoals(ctx context.Context, goals *model.Goals) error
+}
+
+type GoalsRepo struct {
+	repo repository.GoalsRepository
+}
+
+func NewGoalsService(repo repository.GoalsRepository) GoalsService {
+	return &GoalsRepo{repo: repo}
+}
+
+func (s *GoalsRepo) CreateNewGoals(ctx context.Context, goals *model.Goals) error {
+	return s.repo.CreateNewGoals(ctx, goals)
+}
