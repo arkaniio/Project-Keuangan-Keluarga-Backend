e6056d47d81a9b0211c729af6c59a3988885b168
arkaniio <laluahmadarkani@gmail.com>
2026-04-13 20:28:11 +0800

    add new logic for user role logic

--- service/category_service.go
+++ service/category_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"errors"
 	"project-keuangan-keluarga/model"
 	"project-keuangan-keluarga/repository"
 	"project-keuangan-keluarga/utils"
@@ -17,32 +18,44 @@ type CategoryService interface {
 	GetAllCategory(ctx context.Context, params model.PaginationParams) (*model.PaginatedResponse, error)
 }
 
-type repoCategory struct {
-	repo repository.CategoryRepository
+type repoCategoryCombine struct {
+	repoCategory repository.CategoryRepository
+	repoUser     repository.UserRepository
 }
 
-func NewCategoryService(repo repository.CategoryRepository) CategoryService {
-	return &repoCategory{repo: repo}
+func NewCategoryService(repoCategory repository.CategoryRepository, repoUser repository.UserRepository) CategoryService {
+	return &repoCategoryCombine{repoCategory: repoCategory, repoUser: repoUser}
 }
 
-func (s *repoCategory) CreateNewCategory(ctx context.Context, categories *model.Category) error {
-	return s.repo.CreateNewCategory(ctx, categories)
+func (s *repoCategoryCombine) CreateNewCategory(ctx context.Context, categories *model.Category) error {
+
+	users_data, err := s.repoUser.GetUserById(ctx, categories.UserId)
+	if err != nil {
+		return errors.New("Failed to get the users data!")
+	}
+
+	if users_data.Role != "kepala keluarga" {
+		return errors.New("Failed to access this method!")
+	}
+
+	return s.repoCategory.CreateNewCategory(ctx, categories)
+
 }
 
-func (s *repoCategory) UpdateCategory(ctx context.Context, id uuid.UUID, payload model.UpdatePayloadCategory) error {
-	return s.repo.UpdateCategory(ctx, id, payload)
+func (s *repoCategoryCombine) UpdateCategory(ctx context.Context, id uuid.UUID, payload model.UpdatePayloadCategory) error {
+	return s.repoCategory.UpdateCategory(ctx, id, payload)
 }
 
-func (s *repoCategory) DeleteCategory(ctx context.Context, id uuid.UUID) error {
-	return s.repo.DeleteCategory(ctx, id)
+func (s *repoCategoryCombine) DeleteCategory(ctx context.Context, id uuid.UUID) error {
+	return s.repoCategory.DeleteCategory(ctx, id)
 }
 
-func (s *repoCategory) GetCategoryById(ctx context.Context, id uuid.UUID) (*model.Category, error) {
-	return s.repo.GetCategoryById(ctx, id)
+func (s *repoCategoryCombine) GetCategoryById(ctx context.Context, id uuid.UUID) (*model.Category, error) {
+	return s.repoCategory.GetCategoryById(ctx, id)
 }
 
-func (s *repoCategory) GetAllCategory(ctx context.Context, params model.PaginationParams) (*model.PaginatedResponse, error) {
-	items, totalItems, err := s.repo.GetAllCategory(ctx, params)
+func (s *repoCategoryCombine) GetAllCategory(ctx context.Context, params model.PaginationParams) (*model.PaginatedResponse, error) {
+	items, totalItems, err := s.repoCategory.GetAllCategory(ctx, params)
 	if err != nil {
 		return nil, err
 	}
