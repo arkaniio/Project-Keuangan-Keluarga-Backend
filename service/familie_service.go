3d615d4039e9158014f9f6c3f133fc6b5bbe053e
arkaniio <laluahmadarkani@gmail.com>
2026-04-15 11:34:39 +0800

    add: add new familie service

--- service/familie_service.go
+++ service/familie_service.go
@@ -1,1 +1,39 @@
 package service
+
+import (
+	"context"
+	"errors"
+	"project-keuangan-keluarga/model"
+	"project-keuangan-keluarga/repository"
+)
+
+type FamilieService interface {
+	CreateNewFamilie(ctx context.Context, familie *model.Familie) error
+}
+
+type repoCombineFamilieAndUser struct {
+	repoFamilie repository.FamilieRepository
+	repoUser    repository.UserRepository
+}
+
+func NewFamilieService(repoFamilie repository.FamilieRepository, repoUser repository.UserRepository) FamilieService {
+	return &repoCombineFamilieAndUser{
+		repoFamilie: repoFamilie,
+		repoUser:    repoUser,
+	}
+}
+
+func (s *repoCombineFamilieAndUser) CreateNewFamilie(ctx context.Context, familie *model.Familie) error {
+
+	users_data, err := s.repoUser.GetUserById(ctx, familie.Created_By)
+	if err != nil {
+		return errors.New("Failed to get the users data!")
+	}
+
+	if users_data.Id != familie.Created_By {
+		return errors.New("Failed to access this method the id is not same!")
+	}
+
+	return s.repoFamilie.CreateNewFamilie(ctx, familie)
+
+}
